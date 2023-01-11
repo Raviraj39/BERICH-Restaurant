0c077adebaae8ae9381f68033a375b14fad6c657
Raviraj39 <rravirajbujawade@gmail.com>
2023-01-12 01:20:00 +0530

    added all files and nevbar getting ready

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app-navbar">
+      <div className="app-logo">
+        <img src={images.berich} alt="" />
+      </div>
+      <ul className="app-navbar-links">
+        <li className="nav-link">
+          <a href="#home">Home</a>
+        </li>
+        <li className="nav-link">
+          <a href="#about">About</a>
+        </li>
+        <li className="nav-link">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="nav-link">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="nav-link">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="app-navbar-login">
+        <a href="#login" className="p-login">
+          LogIn / SignUP
+        </a>
+        <div></div>
+        <a href="/" className="p-login">
+          Book Table
+        </a>
+      </div>
+      <div className="nav-small">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="nav-smalloverlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              color="#fff"
+              fontSize={27}
+              className="overlay-close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app-small-ul-links">
+              <li>
+                <a href="#home" onClick={() => setToggleMenu(false)}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#about" onClick={() => setToggleMenu(false)}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#menu" onClick={() => setToggleMenu(false)}>
+                  Menu
+                </a>
+              </li>
+              <li>
+                <a href="#awards" onClick={() => setToggleMenu(false)}>
+                  Awards
+                </a>
+              </li>
+              <li>
+                <a href="#contact" onClick={() => setToggleMenu(false)}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
