f888b73699a33e4c4e8212397841d60779f13b1d
Raviraj39 <rravirajbujawade@gmail.com>
2023-01-13 00:38:41 +0530

    getting ready Header

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
+import PropTypes from "prop-types";
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
   </div>
 );
-
+SubHeading.propTypes = {
+  title: PropTypes.node.isRequired,
+};
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app-header">
+    <div className="left-container">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app-header-h1">
+        The Key To <br /> Fine Dining
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, pariatur
+        <br />
+        perspiciatis consequuntur quas repudiandae, id error reprehenderit
+        <br />
+        nesciunt sunt debitis cupiditate maiores.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="right-container">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
