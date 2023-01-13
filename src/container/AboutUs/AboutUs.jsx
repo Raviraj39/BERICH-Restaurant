5dbbc5bacce944d9be23cb2a24519bb547b12568
Raviraj39 <rravirajbujawade@gmail.com>
2023-01-14 00:43:04 +0530

    header Done✅ with responsive

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
-
+import "./AboutUs.css";
+import { images } from "../../constants";
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app-aboutus app__bg flex__center section__padding ">
+    <div className="app-aboutus-overlay flex__center">
+      <img src={images.G} alt="G-main" />
+    </div>
+    <div className="app-aboutus-content flex__center  ">
+      <div className="aboutus-content-aboutus">
+        <h1 className="headtext__cormorant">About us</h1>
+        <img src={images.spoon} alt="spoon-aboutus" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+      <div className="aboutus-knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+      <div className="aboutus-content-history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="spoon-aboutus" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+    </div>
   </div>
 );
 
