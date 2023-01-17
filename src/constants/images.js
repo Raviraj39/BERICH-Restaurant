80b02520f6927a6d22a846a04fe9cbdb7551ebf0
Raviraj39 <rravirajbujawade@gmail.com>
2023-01-17 16:59:25 +0530

    getting change

--- src/constants/images.js
+++ src/constants/images.js
@@ -11,11 +11,6 @@ import menu from "../assets/menu.png";
 import overlaybg from "../assets/overlaybg.png";
 import spoon from "../assets/spoon.svg";
 import findus from "../assets/findus.png";
-import laurels from "../assets/laurels.png";
-import award01 from "../assets/award01.png";
-import award02 from "../assets/award02.png";
-import award03 from "../assets/award03.png";
-import award05 from "../assets/award05.png";
 import sign from "../assets/sign.png";
 import quote from "../assets/quote.png";
 import berich from "../assets/berich.png";
@@ -35,11 +30,6 @@ export default {
   overlaybg,
   spoon,
   findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
   sign,
   quote,
   berich,
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import { meal } from "../../constants";
+import { useState } from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 
-import './Intro.css';
-
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
-
+import "./Intro.css";
+const Intro = () => {
+  const [playVideo, setplayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+  const handleVideo = () => {
+    setplayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <div className="app-videos">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="Video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app-video-overlay flex__center">
+        <div
+          className="app-video-overlay-circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Intro;
